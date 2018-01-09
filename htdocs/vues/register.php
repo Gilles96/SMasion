f5b9c1ec1cb8a6d608eaad2a20b337e3513b73d5
Gilles96 <34276435+Gilles96@users.noreply.github.com>
2018-01-09 14:55:29 +0100

    Update register.php

--- htdocs/vues/register.php
+++ htdocs/vues/register.php
@@ -1,7 +1,30 @@
 <?php 
-/**
-* Vue : register a new user
-*/ckrugcygcehkbcegubegr
+if(!empty($_POST['username']))
+{
+	mysql_connect();
+	mysql_select_db('users');
+	
+	$password = mysql_real_escape_string(htmlspecialchars($_POST['password']));
+	$password2= mysql_real_escape_string(htmlspecialchars($_POST['password2']));
+	
+	if ($password=$password2)
+	{
+		$username= mysql_real_escape_string(htmlspecialchars($_POST['username']));
+		$email= mysql_real_escape_string(htmlspecialchars($_POST['email']));
+		$cmac= mysql_real_escape_string(htmlspecialchars($_POST['cmac']));
+		
+		$hashedpass = password_hash($password, PASSWORD_DEFAULT);
+		
+		mysql_query("INSERT INTO validation VALUES('', '$username', '$hashedpass', '$email', '$cmac')");
+		
+
+
+	}
+	else
+	{
+		echo 'The two passwords does not match ! Try again...' 
+	}
+}
 ?>
 
 <h1><?= $title; ?></h1>
@@ -10,11 +33,21 @@
 
 <form method="POST" action="">
 	
-	<label>Pseudo :</label>
+	<label>Username :</label>
 	<input type="text"  name="username" />
 	
 	<label>Password :</label>
 	<input type="password"  name="password"  />
+	
+	<label>Password2 :</label>
+	<input type="password"  name="password2"  />
+	
+	<label>Email :</label>
+	<input type="email"  name="email"  />
+	
+	<label>Cmac Address :</label>
+	<input type="int"  name="cmac"  />
+	
 
     <button type="submit" name="submit">Register</button>
 
